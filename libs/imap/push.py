00cfdedded5a57e881af4631c29323ac91b7d8a2
Jack Zhuang <zhuangjianguo@me.com>
2018-11-28 19:08:12 +0800

    mail push notification

---
+++ libs/imap/push.py
@@ -0,0 +1,29 @@
+import imaplib
+ 
+
+def push_enable(server, port, email, password):
+	try:
+		connection = imaplib.IMAP4_SSL(server, port)
+		connection.login(email, password)
+		metadata = '(/private/vendor/vendor.dovecot/http-notify "user=" + email + ")'
+		connection._simple_command('SETMETADATA', "", metadata)
+		connection.logout()
+		return 1
+	except Exception as err:
+		print('imap failed: ', err) 
+		return 0
+
+		
+def push_disable(server, port, email, password):
+	try:
+		connection = imaplib.IMAP4_SSL(server, port)
+		connection.login(email, password)
+		metadata = '(/private/vendor/vendor.dovecot/http-notify NIL)'
+		connection._simple_command('SETMETADATA', "", metadata)
+		connection.logout()
+		return 1
+	except Exception as err:
+		print('imap failed: ', err) 
+		return 0
+
+		 
